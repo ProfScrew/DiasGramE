8ba09b0b0ff8c309759f2dcf91b015b8955465af
Stefano Masiero <58975318+SteCodeLevi@users.noreply.github.com>
2021-05-11 13:21:17 +0200

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 
 
@@ -5,8 +6,19 @@
 #include "dais_exc.h"
 int main() {
     try{
-    Tensor t(2,2,2,2.0);
+    Tensor t(3,3,3,2.0);
     //std::cout<<t(1,1,1);
+    //std:: cout<<endl;
+    Tensor c(t);
+    t(1,1,1)=1;
+    Tensor res=c-t;
+    t.stampa_Tensor();
+    std::cout<<endl;
+    std::cout<<endl;
+    c.stampa_Tensor();
+    std::cout<<endl;
+    std::cout<<endl;
+    res.stampa_Tensor();
     }catch( index_out_of_bound err){
         std::cout<<err.msg;
     }
