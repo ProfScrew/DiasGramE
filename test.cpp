cc85dac024af4ca45fec8d6f57d3f2c11cffb7f6
Stefano Masiero <58975318+SteCodeLevi@users.noreply.github.com>
2021-05-13 00:02:33 +0200

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -6,20 +6,26 @@
 #include "dais_exc.h"
 int main() {
     try{
-    Tensor t(3,3,3,2.0);
-    //std::cout<<t(1,1,1);
-    //std:: cout<<endl;
-    Tensor c(t);
-    t(1,1,1)=1;
-    Tensor res=c-t;
-    t.stampa_Tensor();
-    std::cout<<endl;
-    std::cout<<endl;
-    c.stampa_Tensor();
-    std::cout<<endl;
-    std::cout<<endl;
+    Tensor t(3,3,3,2);
+    Tensor s(3,6,3,1);
+    t(1,1,0)=5;
+    t(1,1,1)=10;
+    Tensor res=t.concat(s,1);
     res.stampa_Tensor();
-    }catch( index_out_of_bound err){
+
+
+    /*Tensor a = t.padding(1,1);
+    t.stampa_Tensor();
+    std::cout << std::endl;
+    a.stampa_Tensor();
+    t = a.subset(1,3,1,3,0,5);
+    std::cout << std::endl;
+    t.stampa_Tensor();*/
+    }catch(index_out_of_bound err){
         std::cout<<err.msg;
-    }
-    }
+    }catch( dimension_mismatch err){
+        std::cout<<err.msg;
+    }catch( concat_wrong_dimension err){
+        std::cout<<err.msg;
+    }  
+}
