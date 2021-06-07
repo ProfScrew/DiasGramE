93cb930db8f045c5e6cc4f081367e8f258f90c33
Stefano Masiero <58975318+SteCodeLevi@users.noreply.github.com>
2021-06-07 22:44:00 +0200

    Update dais_exc.h

--- dais_exc.h
+++ dais_exc.h
@@ -6,56 +6,65 @@
 using namespace std;
 
 /* Expections */
-struct index_out_of_bound{
+struct index_out_of_bound
+{
     string msg = "Index out of bound";
     string code = "001";
 };
 
-struct dimension_mismatch{
+struct dimension_mismatch
+{
     string msg = "Dimensions Mismatch";
     string code = "002";
 };
 
-struct unknown_operation{
+struct unknown_operation
+{
     string msg = "Unknown Operation";
     string code = "003";
 };
 
-struct method_not_implemented{
+struct method_not_implemented
+{
     string msg = "Method not implemented";
     string code = "004";
 };
 
-struct tensor_not_initialized{
+struct tensor_not_initialized
+{
     string msg = "Tensor not initilized";
     string code = "005";
 };
 
-struct concat_wrong_dimension{
+struct concat_wrong_dimension
+{
     string msg = "Concatenation on wrong dimension";
     string code = "006";
 };
 
-struct unable_to_read_file{
+struct unable_to_read_file
+{
     string msg = "Unable to read file";
-    string code = "007";   
+    string code = "007";
 };
 
-struct filter_odd_dimensions{
+struct filter_odd_dimensions
+{
     string msg = "The (width and height) dimensions of the filter must be odd";
-    string code = "008";   
+    string code = "008";
 };
 
-struct unable_to_write_file{
+struct unable_to_write_file
+{
     string msg = "Unable to write file";
-    string code = "009";  
+    string code = "009";
 };
 
-struct unknown_exception{
+struct unknown_exception
+{
     string msg = "An error occured";
-    string code = "-1";   
+    string code = "-1";
     /* Utilizzate questa eccezione per qualsiasi tipo di problema non gestito da altre eccezioni */
 };
 
 #endif
-
